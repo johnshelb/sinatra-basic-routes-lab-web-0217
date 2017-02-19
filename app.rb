6db55300d305495473592fbfc64130b593c2e078
johnshelb <jsnd11@gmail.com>
2017-02-18 20:10:18 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+
+
+get "/name" do
+  "My name is John Shelby"
+end
+
+get "/hometown" do
+  "My hometown is NYC"
+end
+
+get "/favorite-song" do
+  "My favorite song is \'Just Like Tom Thumb's Blues\'"
+end
+
+
 end
